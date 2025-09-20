8d6148379fe40f0d38febcd340e763965147ba7b
PW-Thao <vuthaotester1122@gmail.com>
2025-09-20 15:30:51 +0700

    P3

---
+++ 01-object.js
@@ -0,0 +1,54 @@
+// 1.1
+// const car = { make:'Toyota' ,
+//     model:'Coralla' ,
+//     year : 2021
+// }
+// console.log(car['year'])
+
+// 1.2
+// const person = { name:'Thao',
+//     address: {
+//        street:'Tran Phu',
+//        city  :'Hanoi',
+//        country:'Vietnam'
+//     }
+// }
+// console.log(person.address['street'])
+
+// 1.3
+// const student = {
+//     name : 'Thao',
+//     grades : {
+//         math : 9,
+//         english : 10, 
+//     }  
+// }
+// console.log(student.grades['math']);
+
+// 1.4
+// const settings = {
+//     volume : 80,
+//     brightness: 75
+// }
+// settings.volume = 90;
+// console.log(settings)
+
+// 1.5
+// const bike = {}
+// bike.color = 'red';
+// console.log(bike);
+
+// 1.6
+// const employee = {
+//     name : 'thao',
+//     age  : 25
+// }
+// delete employee.age
+// console.log(employee);
+
+1.7
+const school = {
+    classA:["An", "Bình", "Châu"],
+    classB:["Đào","Hương","Giang"]
+}
+console.log(school);
